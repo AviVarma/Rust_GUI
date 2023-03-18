d3fe439070dd5a89d59a27977081c738dd8ff8d2
Avi Varma <avi.varma@ymail.com>
2023-03-18 22:49:11 +0000

    Simple Gui with a list of dummy articles.

--- rust_gui/src/main.rs
+++ rust_gui/src/main.rs
@@ -1,7 +1,46 @@
-use eframe::run_native;
+use eframe::egui::{self, collapsing_header::HeaderResponse};
 
-struct RustyHeadlines;
+#[derive(Default)]
+struct RustyHeadlines {
+    articles: Vec<NewsCardData>,
+}
 
-// fn main() {
-//     run_native(app, native)
-// }
+struct NewsCardData {
+    title: String,
+    description: String,
+    url: String,
+}
+
+impl RustyHeadlines{
+    fn new(cc: &eframe::CreationContext<'_>) -> Self {
+        let iter = (0..20).map(|a| NewsCardData {
+            title: format!("Article {}", a),
+            description: format!("Description of article {}", a),
+            url: format!("https://rusty.rs/{}", a),
+        });
+
+        Self::default();
+
+        RustyHeadlines {
+            articles: Vec::from_iter(iter),
+        }
+    }
+}
+
+impl eframe::App for RustyHeadlines {
+    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            for a in &self.articles{
+                ui.label(&a.title);
+                ui.label(&a.url);
+                ui.label(&a.description);
+                
+            }
+        });
+    }
+}
+
+fn main() {
+    let native_options = eframe::NativeOptions::default();
+    eframe::run_native("Rusty Headlines", native_options, Box::new(|cc| Box::new(RustyHeadlines::new(cc))));
+}
